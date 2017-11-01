42c0331e60eaae73ec65e5e97c87b780c8f6f587
Eric Hugoson <eric@hugoson.org>
2017-11-01 18:14:03 +0100

    fixed import

--- minerva/__init__.py
+++ minerva/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['parse_mineva_map', 'parse_taxonomy'] 
 
-from parse_minerva_map import parseMapFile
-from parse_taxonomy import parseTaxonomy
+from .parse_minerva_map import parseMapFile
+from .parse_taxonomy import parseTaxonomy
