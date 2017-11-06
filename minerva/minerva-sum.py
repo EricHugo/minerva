b2ecbf8905a14e1c1f9e8a51ce809d5d4e406b87
Eric Hugoson <eric@hugoson.org>
2017-11-06 17:38:56 +0100

    added frequency summary function

--- minerva/minerva-sum.py
+++ minerva/minerva-sum.py
@@ -25,41 +25,79 @@ def _worker(minerva_table, search_set):
     print(search_lists)
     found_list = [ ''.join(found) for found in parseMapFile(minerva_table, 
         search_lists[0], search_lists[1], search_lists[2]) ]
-    #print(found_list)
+    print(found_list)
     all_search_lists = copy.deepcopy(search_lists)
     all_search_lists[0][0] = "name"
     all_search_lists[1][0] = ".*"
     all_found_list = [ ''.join(all_found) for all_found in parseMapFile(minerva_table, 
         all_search_lists[0], all_search_lists[1], all_search_lists[2]) ]
     #print(all_found_list)
-    found_dict[search_lists[1][1]].append(found_list)
-    found_dict[search_lists[1][1]].append(all_found_list)
+    ind = len(search_lists[0]) - 1
+    found_dict[search_lists[ind][ind]].append(found_list)
+    found_dict[search_lists[ind][ind]].append(all_found_list)
+    selector = search_lists[1][0]
     try:
-        secondary_selection = search_lists[1][0]
+        secondary_selector = search_lists[0][1]
     except IndexError:
-        secondary_selection = None
+        secondary_selector = None
     #print(found_dict)
-    return found_dict, secondary_selection
+    return found_dict, selector, secondary_selector
 
-def create_bar_summary(found_v_total_set, secondary_title):
+def create_stacked_summary(found_v_total_set, secondary_title):
     fig, ax = plt.subplots()
     sep = 0
+    width = 0.7 / len(found_v_total_set)
+    print(found_v_total_set)
     for found_v_total in found_v_total_set:
+        print(width)
         #print(found_v_total)
         found_no = [ len(set(found[0])) for name, found in found_v_total.items() ]
         remaining_no = [ len(set(total[1])) - len(set(total[0])) for name, total in 
                 found_v_total.items() ]
         labels = [ name for name, found in found_v_total.items() ]
         ind = np.arange(len(found_v_total))
-        p1 = ax.bar(ind + sep, remaining_no, color='#348abd', width=0.15, )
-        p2 = ax.bar(ind + sep, found_no, width=0.15, color='#e24a33', 
+        p1 = ax.bar(ind + sep, remaining_no, color='#348abd', width=width)
+        p2 = ax.bar(ind + sep, found_no, width=width, color='#e24a33', 
                 bottom=remaining_no)
-        sep += 0.19
+        print(sep)
+        sep += width + 0.05
     ax.set_xticks(ind + sep / 2)
     ax.set_xticklabels(labels, rotation=45, fontsize=9, ha='right')
     plt.ylabel('Taxa')
     #
     plt.legend((p1[0], p2[0]), ('Total', 'Found'))
+    plt.show()
+    return
+
+def create_frequency_summary(found_v_total_set, selectors, secondary_title):
+    sep = 0
+    found_copy_numbers = []
+    #print(found_v_total_set)
+    for found_v_total in found_v_total_set:
+        #print(found_v_total)
+        found_all = [ found[0] for name, found in found_v_total.items() if found[0] ]
+        #print(found_all)
+        # necessary because nested list
+        for found in found_all:
+            found_numbers = [ found.count(num) for num in set(found) ] 
+            #print(found_numbers)
+            found_copy_numbers.append(found_numbers)
+            print(found_copy_numbers)
+    if not secondary_title:
+        labels = [ sel for sel in selectors ]
+        print(labels)
+    else:
+        labels = []
+        for found_v_total in found_v_total_set:
+            sel = [ name for name, found in found_v_total.items() if found [0]]
+            print(labels)
+            labels = labels + sel
+    fig, ax = plt.subplots(figsize=(9, 6)) 
+    plt.boxplot(found_copy_numbers, sym='+')
+    ax.set_xticklabels(labels, rotation=45, ha='right', fontsize=8)
+    ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+            alpha=0.5)
+    ax.set_ylabel('Copy number')
     plt.show()
     return
 
@@ -86,19 +124,20 @@ def main():
         with open(args.file) as queries:
             found_series = defaultdict(list)
             found_series_set = []
-            old_ss = None
+            selector_set = []
             for each in queries:
-                found_set, secondary_selection = _worker(args.minervaTable, 
+                found_set, selector, secondary_selection = _worker(args.minervaTable, 
                         each.strip().split('\t'))
                 print(secondary_selection)
-                if secondary_selection != old_ss and old_ss:
+                if selector not in selector_set and selector_set:
                     found_series_set.append(found_series)
                     found_series = defaultdict(list)
+                selector_set.append(selector)
                 found_series = {**found_series, **found_set}
-                old_ss = secondary_selection
             found_series_set.append(found_series)
             print(len(found_series_set))
-    create_bar_summary(found_series_set, secondary_selection)
+    #create_stacked_summary(found_series_set, secondary_selection)
+    create_frequency_summary(found_series_set, selector_set, secondary_selection)
 
 if __name__ == "__main__":
     main()
