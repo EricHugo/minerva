fd65f940e03d1353ad06905fca2fbfb1bcd403a7
Eric Hugoson <eric@hugoson.org>
2017-11-23 18:45:18 +0100

    implemented handling of no grouping for stacked plots

--- minerva/minerva-sum.py
+++ minerva/minerva-sum.py
@@ -23,7 +23,20 @@ except ImportError:
 
 matplotlib.style.use('ggplot')
 
-def gather_minerva_data(minerva_table, search_lists, gather_negatives=False):
+def gather_minerva_data(minerva_table, search_lists, gather_negatives=False,
+                        unique_flag=False):
+    """Gathers data from a minerva DB file according to columns and 
+    search query in search_lists.
+    
+    .. Example:
+        gather_minerva_data("minervaDB", search_lists)
+        
+        returns a list of dicts in which each key is the last selector 
+        and value is a list of whatever what was matched and requested,
+        also returns a nested list of primary selectors of corresponding 
+        length to list of dicts, finally returns the last query column. 
+
+    """
     all_found_list = []
     selectors = []
     print(search_lists)
@@ -33,15 +46,15 @@ def gather_minerva_data(minerva_table, search_lists, gather_negatives=False):
         found_dict = defaultdict(list)
         cprint(search_list, "red")
         query_list = [ query.split(' ') for query in search_list ]
-        print(query_list[2])
         found_list = [ found for found in parseMapFile(minerva_table, 
-            query_list[0], query_list[1], query_list[2], unique=True, 
-            ) ]
-        print(found_list)
+                        query_list[0], query_list[1], query_list[2], 
+                        unique=unique_flag)
+                     ]
+        #print(found_list)
         found_dict[query_list[1][-1]].append(found_list)
         all_found_list.append(found_dict)
         selectors.append(query_list[1][0])
-        print(selectors)
+        #print(selectors)
     # extract all results, assuming it matches index 1+, to get a total possible
     #print(all_found_list)
     # specify last index of index 1 as the name for the particular list
@@ -71,8 +84,9 @@ def _worker(minerva_table, search_set):
     all_search_lists[0][0] = "name"
     all_search_lists[1][0] = ".*"
     # extract all results, assuming it matches index 1+, to get a total possible
-    all_found_list = [ ''.join(all_found) for all_found in parseMapFile(minerva_table, 
-        all_search_lists[0], all_search_lists[1], all_search_lists[2]) ]
+    all_found_list = [ ''.join(all_found) for all_found in 
+            parseMapFile(minerva_table, all_search_lists[0], all_search_lists[1], 
+                all_search_lists[2]) ]
     #print(all_found_list)
     # specify last index of index 1 as the name for the particular list
     found_dict[search_lists[1][-1]].append(found_list)
@@ -91,60 +105,72 @@ def create_stacked_summary(found_v_total_set, secondary_title):
     fig, ax = plt.subplots()
     sep = 0
     width = 0.7 / len(found_v_total_set)
-    print(type(found_v_total_set))
-    for each in found_v_total_set:
-        print([ ind for ind, val in each.items() ])
+    # check if length of the set is only 1, if so assign label to each
+    # key within the dict 
+    if len(found_v_total_set[0]) == 1:
+        label = []
+        indic = []
+    # here loop all different results dicts within the set
     for found_v_total in found_v_total_set:
-        #print(found_v_total)
+        # determine number of positives
         found_no = [ len(set(found[0])) / len(set(found[1])) for name, found in found_v_total.items() ]
+        # determine length of the the total - positives
         remaining_no = [ (len(set(total[1])) - len(set(total[0]))) / len(set(total[1])) for name, total in 
                 found_v_total.items() ]
         labels = [ name for name, found in found_v_total.items() ]
         ind = np.arange(len(found_v_total))
-        print(labels)
-        print(ind)
         p1 = ax.bar(ind + sep, remaining_no, color='#348abd', width=width)
         p2 = ax.bar(ind + sep, found_no, width=width, color='#e24a33', 
                 bottom=remaining_no)
+        if len(found_v_total) == 1:
+            label.append(''.join(labels))
+            indic.append(ind + sep)
+            ax.set_xticks(indic)
+            ax.set_xticklabels(label, rotation=45, fontsize=9, ha='right')
         sep += width + 0.05
     # set xticks to be middle for each set, subtracts width and 0.05 to
     # compensate last increment
-    ax.set_xticks(ind + (sep - width - 0.05) / 2)
-    ax.set_xticklabels(labels, rotation=45, fontsize=9, ha='right')
+    #ax.set_xticks(ind + (sep - width - 0.05) / 2)
+    #ax.set_xticklabels(labels, rotation=45, fontsize=9, ha='right')
     plt.ylabel('Taxa')
-    #
     plt.legend((p1[0], p2[0]), ('Negative', 'Postive'))
     plt.show()
     return
 
-def create_frequency_summary(minerva_table, search_sets):
+def create_frequency_summary(minerva_table, search_sets, unique=False):
     # try pandas and seaborn for grouping
     df = pd.DataFrame()
     all_found_matches, selectors, secondary_selector = gather_minerva_data(
-            minerva_table, search_sets)
+            minerva_table, search_sets, unique_flag=unique)
     for selector, found_v_total in zip(selectors, all_found_matches):
         print(selector)
         data = {}
         # questionable whether the if clause should be included, possibly better 
         # to handle empty list counts
-        found_all = [ found[0] for name, found in found_v_total.items() if found[0] ]
+        found_all = [ found[0] for name, found in found_v_total.items() if 
+                      found[0] ]
         labels =  [ name for name, found in found_v_total.items() if found[0]] 
         #print(found_all)
         # necessary because nested list
         for found, label in zip(found_all, labels):
-            print(label)
-            print(found)
+            #print(label)
+            #print(found)
+            # flatten nested list
             found_flat = list(chain(*found))
+            # if contents are not numerical, resort to counting copies of 
+            # strings
             try:
                 found_numbers = [ float(num) for num in found_flat ]
+                values = "values"
             except ValueError:
-                found_numbers = [ found_flat.count(num) for num in set(found_flat) ] 
+                found_numbers = [ found_flat.count(num) for num in set(found_flat) ]
+                values = "Number of copies"
             if secondary_selector:
                 second_sel = [ label for i in range(len(found_numbers)) ]
                 data[secondary_selector] = second_sel
             prim_sel = [ selector for i in range(len(found_numbers)) ]
-            data["values"] = found_numbers
-            data["Selector"] = prim_sel
+            data[values] = found_numbers
+            data["Selectors"] = prim_sel
             #print(found_numbers) 
             #print(label)
             df_new = pd.DataFrame(data)
@@ -152,19 +178,20 @@ def create_frequency_summary(minerva_table, search_sets):
     print(df)
     print(secondary_selector)
     if secondary_selector:
-        bp = sns.boxplot(x='Selector',y='values',data=df,hue=secondary_selector, width=0.7)
+        bp = sns.boxplot(x='Selectors',y=values,data=df,hue=secondary_selector, 
+                width=0.7)
     else:
-        bp = sns.boxplot(x='Selector',y='values',data=df, width=0.7)
+        bp = sns.boxplot(x='Selectors',y=values,data=df, width=0.7)
     plt.show()
     return
 
-def create_residual_summary(minerva_table, search_sets):
+def create_residual_summary(minerva_table, search_sets, unique=False):
     df = pd.DataFrame()
     search_lists = []
     print(search_sets)
     # 
     all_found_matches, selectors, secondary_selector = gather_minerva_data(
-            minerva_table, search_sets)
+            minerva_table, search_sets, unique)
     x, y = search_sets[0][2].split(' ')
     for selector, found_match in zip(selectors, all_found_matches):
         data = {}
@@ -174,7 +201,11 @@ def create_residual_summary(minerva_table, search_sets):
         # necessary because nested list
         for found, label in zip(found_all, labels):
             print(label)
-            x_numbers = [ float(num[0]) for num in found ] 
+            try:
+                x_numbers = [ float(num[0]) for num in found ] 
+            except ValueError:
+                found_flat = list(chain(*found))
+                x_numbers = [ found_flat.count(num) for num in found_flat ]
             y_numbers = [ float(num[1]) for num in found ] 
             second_sel = [ label for i in range(len(x_numbers)) ]
             prim_sel = [ selector for i in range(len(x_numbers)) ]
@@ -183,6 +214,9 @@ def create_residual_summary(minerva_table, search_sets):
             data[secondary_selector] = second_sel
             data["Selector"] = prim_sel
             print(data)
+            for key, val in data.items():
+                print(key)
+                print(len(val))
             #print(found_numbers) 
             #print(label)
             df_new = pd.DataFrame(data)
@@ -190,7 +224,8 @@ def create_residual_summary(minerva_table, search_sets):
     print(df)
     print(secondary_selector)
     if secondary_selector:
-        pp = sns.pairplot(data=df, x_vars=x, y_vars=y, hue=secondary_selector, kind='reg') 
+        pp = sns.pairplot(data=df, x_vars=x, y_vars=y, hue=secondary_selector, 
+                kind='reg') 
     else:
         pp = sns.pairplot(data=df, x_vars=x, y_vars=y, kind='reg') 
     plt.show()
@@ -219,6 +254,8 @@ def main():
     numbers of matching..........""")
     parser.add_argument("--normalize", action='store_true', help="""Attempts to 
     normalize data plotted.""")
+    parser.add_argument("--unique", action='store_true', help="""Discards
+    non-unique output from the minerva DB.""")
     parser.add_argument("-f", "--file", required=False, help="""File in which 
     mutiple seperate queries and columns summaries are requested. Structured with 
     seperate queries with space, queries and columns seperated by tab, and 
@@ -228,19 +265,21 @@ def main():
 
     if args.pairplot:
         with open(args.file) as queries:
-            search_sets = [ search_set.strip().split('\t') for search_set in queries ]
-        create_residual_summary(args.minervaTable, search_sets)
-        sys.exit()
+            search_sets = [ search_set.strip().split('\t') for search_set in 
+                            queries ]
+        create_residual_summary(args.minervaTable, search_sets, 
+                                unique=args.unique)
 
     if args.boxplot:
         with open(args.file) as queries:
-            search_sets = [ search_set.strip().split('\t') for search_set in queries ]
-        create_frequency_summary(args.minervaTable, search_sets)
-        sys.exit()
+            search_sets = [ search_set.strip().split('\t') for search_set in 
+                            queries ]
+        create_frequency_summary(args.minervaTable, search_sets, 
+                                 unique=args.unique)
 
 
     # Garbage temp code below
-    if args.stacked or args.boxplot:
+    if args.stacked:
         if args.file:
             with open(args.file) as queries:
                 found_series = defaultdict(list)
