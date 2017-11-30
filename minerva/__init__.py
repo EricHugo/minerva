206362a2a293f98393bef93f56f32622ed088c49
Eric Hugoson <eric@hugoson.org>
2017-11-30 21:30:36 +0100

    added find_gene_neighbourhood module

--- minerva/__init__.py
+++ minerva/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['parse_mineva_map', 'parse_taxonomy'] 
+__all__ = ['parse_mineva_map', 'parse_taxonomy', 'find_gene_neighbourhood'] 
 
-from .parse_minerva_map import parseMapFile
-from .parse_taxonomy import parseTaxonomy
+from parse_mineva_map import parseMapFile
+from parse_taxonomy import parseTaxonomy
+from find_gene_neighbourhood import findGeneNeighbourhood
