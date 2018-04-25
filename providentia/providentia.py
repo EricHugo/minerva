dd9c6cc358e1cdf3126282b3ec52cbb2f0ec860a
Eric Hugoson <eric@hugoson.org>
2018-04-25 16:55:05 +0200

    major functional overhaul

--- providentia/providentia.py
+++ providentia/providentia.py
@@ -1,97 +1,65 @@
 #! /usr/bin/env python
 
 from contextlib import contextmanager
+from termcolor import cprint
 from scipy import stats
+from collections import OrderedDict
 import pandas as pd
 import multiprocessing as mp
+import multiprocessing.pool
 import sys
 import argparse
 import numpy as np
 import csv
+import math
+import time
 
-def _worker(task, df_minerva, headers, selector, q):
-    # here we determine which function to call
-    # Outer_selection (e.g. RAYT1_pruned), inner_selection (e.g. Genus), target
-    #  (e.g. Forward_distance)
-    out = {}
-    results = {}
-    print(task)
-    group, query = task
-    print(group)
-    # always remove any "-" entries as these represent no value in minerva
-    print(len(df_minerva))
-    df_valid = df_minerva[~df_minerva[query].str.match('-')].copy()
-    print(len(df_valid))
-    print(df_valid.dtypes)
-    try:
-        df_valid[query] = pd.to_numeric(df_valid[query], errors="raise")
-    except ValueError:
-        string_counts(df_valid, group, query)
+try:
+    from recurse_database import recurse_database
+except ImportError:
+    from .recurse_database import recurse_database
+
+def _worker(df, init_header, headers, q, names_column, threads, depth):
+    # here we determine how to recurse down the list
+    # having received a init_header we start with significane testing it
+    # then recurse down once level for each subgroup within init_header
+    # -> testing -> recurse down -> testing down to len(header)
+    # start looping back
+    subgroups = df[init_header].unique()
+    print(subgroups)
+    print(threads)
+    pool = mp.Pool(processes=threads)
+    jobs = []
+    for subgroup in subgroups:
+        job = pool.apply_async(_init_run, (df, init_header, headers, subgroup, 
+                                names_column, q))
+        #job = pool.apply_async(__dummy, (subgroup, q))
+        jobs.append(job)
+        #print('started')
+    for job in jobs:
+        job.get()
+    pool.close()
+    pool.join()
+    cprint("done?", "red")
+    return 
+
+def _init_run(df, init_header, headers, init_slice, names_column, q):
+    """Initiates the recurse run, serves to allow multi-processing from 
+    within the _worker child process."""
+    sub_df = df[df[init_header].str.match(init_slice)]
+    recurse_d = recurse_database(sub_df, init_header, headers, OrderedDict(
+                                {init_header: init_slice}), names_column, df, q=q)
+    recurse_d.__next__()
+    return
+
+def __dummy(num, q):
+    """Dummy process, repeatedly prints given variable and attempts to queue
+    in given q"""
+    while True:
+        print(num)
+        q.put(num)
+        time.sleep(2)
         return
-    df_valid.dropna(subset=[query], inplace=True)
-    print(len(df_valid))
-    print(df_valid.dtypes)
-    all_values = df_valid[query].dropna().values
-    #print(type(all_values))
-    # here get all unique values in group column -> iterate over each
-    # comparing with all_values in scipy ttest
-    subgroups = df_valid[group].unique()
-    for subgroup in subgroups:
-        results[subgroup] = pandas_ttest(df_valid, all_values, group, query, 
-                                        subgroup)
-    print(results)
-    out[selector] = results
-    q.put(out)
-    return
-
-def string_counts(df, group, query_group):
-    all_values = {}
-    subgroups = df[group].unique()
-    for subgroup in subgroups:
-        sub_df = slice_dataframe(df, group, subgroup)
-        print(subgroup)
-        value = sub_df.groupby(query_group)[query_group].count()
-        print(value.values)
-        all_values[subgroup] = int(value.values)
-    all_counts = all_values.values()
-    print(all_counts)
-    for subgroup, count in all_values.items():
-        # can't simply find outlier counts, may have larger number of organisms
-        # contributing to large counts
-        # have to normalise
-        pass
-    return
-
-def slice_dataframe(df, group, subgroup):
-    sub_DB = df[df[group].str.match(subgroup)]
-    return sub_DB
-
-def transform_dataframe(df, query_group):
-    # converts query column to numbers
-    # either by counts or converting already numerical
-    df_valid = df_minerva[~df_minerva[query_group].str.match('-')].copy()
-    print(len(df_valid))
-    print(df_valid.dtypes)
-    try:
-        df_valid[query_group] = pd.to_numeric(df_valid[query], errors="raise")
-        df_valid.dropna(subset=[query_group], inplace=True)
-        values = df_valid[query_group].dropna().values
-    except ValueError:
-        pass
-    print(len(df_valid))
-    print(df_valid.dtypes)
-    return
-
-def pandas_ttest(df, all_values, group, query, subgroup=None):
-    if subgroup:
-        sub_DB = df[df[group].str.match(subgroup)]
-    else:
-        sub_DB = df
-    sub_values = sub_DB[query].values
-    print(sub_values)
-    ttest = stats.ttest_ind(all_values, sub_values, equal_var=False, 
-                        nan_policy='raise')
-    return ttest
 
 def _listener(q, outfile='-'):
     # we'll need a listener to safely output results from seperate
@@ -99,8 +67,18 @@ def _listener(q, outfile='-'):
     with open_stdout(outfile) as handle:
         while True:
             out_object = q.get()
-            if out_object == "done":
-                break
+            cprint(out_object, "green")
+            if type(out_object) is tuple:
+                stat = str(out_object[0])
+                slices = str(out_object[1])
+                with open("result/results", 'a') as f:
+                    f.write(stat + ' ' + slices + '\n')
+            try:
+                if out_object == "done":
+                    break
+            except ValueError:
+                # dataframes
+                continue
             if out_object is type(dict):
                 pass
     return
@@ -121,41 +99,84 @@ def open_stdout(outfile):
         if handle is not sys.stdout:
             handle.close()
 
+def optimal_ints(total, div):
+    """Produces an optimal list of integers given a total, a set size
+    in descending order. If total < set size, populates remaining with
+    1s."""
+    opt_set = []
+    while div: 
+        quotient = total / div
+        up_round = math.ceil(quotient)
+        if up_round <= 0:
+            # fill
+            [ opt_set.append(1) for i in range(div) ]
+            break
+        div -= 1
+        total -= up_round
+        opt_set.append(up_round)
+    return opt_set
+
+class NoDaemonProcess(mp.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+class NoDaemonProcessPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
+
 def main():
     parser = argparse.ArgumentParser(description="""Attempts to find deviating 
             groups within a database produced based on all metrics present.
             Essentially performs an all v. all comparison.""")
 
     parser.add_argument("minervaDB", help="A database file produced by minerva")
+    parser.add_argument("names_column", help="""The name of the column containing 
+                        unique identifiers per organism""")
+    parser.add_argument("-c", "--columns", help="""Specify which columns will be 
+                        included in the analysis""")
+    parser.add_argument("-e", "--entry_columns", help="""Specify which columns to
+                        slice by initially.""")
+    parser.add_argument("-d", "--depth", default=None, type=int,
+                        help="""Specify maximum recursive depth that will be
+                        descended""")
     parser.add_argument("-t", "--threads", default=1, type=int, help="""Define 
                         number of threads to be used""")
 
     args = parser.parse_args()
-    with open(args.minervaDB) as DB:
-        headers = DB.readline().strip().split()
-        print(headers)
+    try:
+        with open(args.columns) as cols:
+            headers = cols.readline().strip().split('\t')
+    except TypeError:
+        with open(args.minervaDB) as DB:
+            headers = DB.readline().strip().split('\t')
+    print(headers)
 
+    print(args.entry_columns)
+    if args.entry_columns:
+        with open(args.entry_columns) as entry:
+            entry_set = entry.readline().strip().split('\t')
     df_minerva_all = pd.read_csv(args.minervaDB, sep='\t')
-    print(df_minerva_all.tail())
-    outer_groups = ['RAYT1_pruned',]# 'RAYT2_pruned','RAYT3_pruned','RAYT4_pruned', 
-
-    print(df_minerva_all.dtypes)
+    names_column = args.names_column
 
     # define tasks to be done, seperate each into multiprocessing
-    tasks = [["Genus", "Match"]]
     manager = mp.Manager()
     q = manager.Queue()
-    pool = mp.Pool(processes=args.threads + 1)
+    # init pool as non-daemon to allow for spawning new child processes within
+    pool = NoDaemonProcessPool(processes = args.threads + 1)
     listener = pool.apply_async(_listener, (q,))
 
     jobs = []
-    for outer_group in outer_groups:
-        print(outer_group)
-        if outer_group:
-            outer_df_minerva = df_minerva_all.groupby('Match').get_group(outer_group)
-        for task in tasks:
-            job = pool.apply_async(_worker, (task, outer_df_minerva, headers, outer_group, q))
-            jobs.append(job)
+    opt_threads = optimal_ints(args.threads, len(entry_set))
+    print(opt_threads)
+    print(len(opt_threads))
+    for init_header, threads in zip(entry_set, opt_threads):
+        job = pool.apply_async(_worker, (df_minerva_all, init_header, headers, q, 
+                                         names_column, threads, args.depth))
+        jobs.append(job)
+        #break
 
     for job in jobs:
         job.get()
--- providentia/recurse_database.py
+++ providentia/recurse_database.py
@@ -1,23 +1,45 @@
 #/usr/bin/env python
 
 from scipy import stats
+from termcolor import cprint
+from collections import OrderedDict
 import pandas as pd
 import numpy as np
 import itertools
 import sys
+import random
+
+try:
+    from recurse_listener import recurse_listener
+except ImportError:
+    from .recurse_listener import recurse_listener
 
 class recurse_database():
-    def __init__(self, df, init_header, headers, names_column):
-        self.df = df
+    def __init__(self, df, init_header, headers, init_slice, names_column, 
+                 parent_df=None, max_depth=0, q=None):
+        self.q = q
+        try:
+            if not parent_df:
+                self.df = self.slice_df(df, init_header, init_slice)
+                self.parent_df = df
+        except ValueError:
+            self.df = df
+            self.parent_df = df
         self.init_header = init_header.split()
         self.headers = headers
         self.names = names_column
+        self.listener = recurse_listener()
+        self.slices = init_slice
+        # initialise the listerner with next()
+        next(self.listener)
+        self.max_depth = max_depth
+        self.current_depth = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return self.recurse(self.df, self.init_header)
+        return self.recurse(self.df, self.slices, self.init_header, self.parent_df)
 
     def __level__(self):
         return self.headerlist, self.slicelist
@@ -27,24 +49,123 @@ class recurse_database():
         return sub_df
 
     def get_headers(self, current_headers):
+        """Using a list of headers, remove from total set and return a new 
+        set with one more header"""
         num_headers = len(current_headers)
-        print(current_headers)
+        #print(current_headers)
         headers = self.headers
         headers = [ head for head in headers if not head in current_headers ]
-        for header_permutation in itertools.permutations(headers, 1):
-            yield header_permutation 
-        return
-
-    def recurse(self, df, current_headers):
-        for header in self.get_headers(current_headers):
+        #print("new")
+        #print(headers)
+        # currently uses permutations, which is a point of randomisation
+        # avoid randomisation?
+        header_permutations = [ header for header in itertools.permutations(headers, 1) ]
+        #print(header_permutations)
+        return header_permutations
+        
+    def recurse(self, df, slices, current_headers, parent_df=None):
+        headers = self.get_headers(current_headers)
+        #print("headers")
+        #print(headers)
+        ident = random.random()
+        #print("new_")
+        for header in headers:
             new_headers = current_headers + list(header)
-            print(new_headers)
-            print(header)
             self.headerlist = new_headers
             subgroups = df[new_headers[-1]].unique()
-            print(subgroups)
+            #print("headers")
+            #print(new_headers)
+            #print(ident)
+            #print(slices)
             for subgroup in subgroups:
-                self.slicelist = None
-                sub_df = self.slice_df(df, new_headers[-1], subgroup) 
-                self.recurse(df=sub_df, current_headers=new_headers)
-                return sub_df
+                slices[new_headers[-1]] = subgroup
+                # here send to recurse_listener coroutine 
+                # this will pause execution and handle results
+                # resume upon completed yield loop
+                try:
+                    sub_df = self.slice_df(df, new_headers[-1], subgroup) 
+                    df, df_type, result = self.call_listener(df, slices, new_headers,
+                                                 parent_df)
+                    if result:
+                        self.q.put(result)
+                except AttributeError:
+                    # take a look at parent_df vs pre-slice df. should parent be
+                    # the pre-sliced rather than from prev recurse
+                    sub_df = df
+                    df, df_type, result = self.call_listener(df, slices, new_headers,
+                                                 parent_df)
+                    # remove last slice. Necessary?
+                    if result:
+                            self.q.put(result)
+                    del slices[new_headers[-1]]  
+                    if df_type == "num":
+                        break
+                    continue
+                # if there are not enough entries left in slice
+                # or they are of numerical type
+                # do not recurse
+                if df_type == "num":
+                    del slices[new_headers[-1]]
+                    break
+                    #continue
+                #self.test.append(sub_df)
+                self.recurse(df=sub_df, slices=slices, current_headers=new_headers,
+                             parent_df=df)
+                # clean last entry post-recurse
+                del slices[new_headers[-1]]
+        return 
+
+    def call_listener(self,df, slices, new_headers, parent_df):
+        """Calls the recurse_listener module"""
+        valid_df, df_type = self.transform_dataframe(df, new_headers[-1])
+        result = self.listener.send((valid_df, slices, new_headers, df_type, parent_df))
+        return valid_df, df_type, result
+
+    def transform_dataframe(self, df, query_group):
+        """Removes empty values from query_group column ('-'), and attempts
+        to convert the column to numerical. If not numerical returns type
+        "str"."""
+        try:
+            # removes '-' entries, may not be desirable in all cases?
+            ## add skip for when query is '-'
+            # is necessary as long as we rely on on exception to determine
+            # string / numeric
+            try:
+                df_valid = df[~df[query_group].str.match('-')].copy()
+            except AttributeError:
+                df_valid = df.copy()
+            #print("received top")
+            #print(query_group)
+            #print(len(df_valid))
+            #print(df_valid[query_group].dtypes)
+            if df_valid[query_group].dtypes == "bool":
+                #print("bool")
+                raise ValueError
+            # attempt to convert query column to numeric values
+            df_valid[query_group] = pd.to_numeric(df_valid[query_group], 
+                                                  errors="raise")
+            # remove N/A from column
+            df_valid.dropna(subset=[query_group], inplace=True)
+            # check if actual values remain in the query group
+            values = df_valid[query_group].dropna().values
+            # if there are no values, implies dataframe for the group is only '-'
+            if not values.any():
+                #print("no values")
+                #print(len(df))
+                #print(df[query_group])
+                # if we raise ValueError here -> will be treated as strings
+                # therefore num of copies
+                raise ValueError
+            #print(values)
+            df_type = "num"
+        except ValueError:
+            df_valid = df.copy()
+            df_valid[query_group] = df[query_group].astype(str)
+            df_type = "str"
+            #print("received exception")
+        #print(len(df_valid))
+        #print(df_valid.dtypes)
+        #print(df_valid)
+        return df_valid, df_type
+
+
