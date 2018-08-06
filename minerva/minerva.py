6487a4109b46a7fb0f66cc30a549b06a40a06303
eric.hugoson <hugoson@evolbio.mpg.de>
2018-08-06 14:31:58 +0200

    general linting

--- minerva/minerva.py
+++ minerva/minerva.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from Bio import SeqIO
-from micomplete import calcCompleteness
-#from micomplete import micomplete
-from contextlib import contextmanager
-from distutils import spawn
-from collections import defaultdict
-from itertools import zip_longest
-from ftplib import FTP
-from datetime import datetime
-from termcolor import cprint
-from pathlib import Path
 import sys
 import mmap
 import re
@@ -24,6 +13,17 @@ import subprocess
 import tarfile
 import hashlib
 import subprocess
+from Bio import SeqIO
+from micomplete import calcCompleteness
+#from micomplete import micomplete
+from contextlib import contextmanager
+from distutils import spawn
+from collections import defaultdict
+from itertools import zip_longest
+from ftplib import FTP
+from datetime import datetime
+from termcolor import cprint
+from pathlib import Path
 
 # import dev version of miComplete
 import importlib.util
@@ -51,7 +51,7 @@ try:
 except FileExistsError:
     pass
 
-def _worker(fasta, seqType, raw_name, hmm, q, gen_directory, tax, evalue=1e-20, 
+def _worker(fasta, seqType, raw_name, hmm, q, gen_directory, tax, evalue=1e-20,
             crispr=False, outfile=None):
     #tax = parseTaxonomy()
     if seqType == "faa":
@@ -114,32 +114,32 @@ def _worker(fasta, seqType, raw_name, hmm, q, gen_directory, tax, evalue=1e-20,
         taxid = tax.find_taxid(raw_name)
         if taxid:
             lineage = tax.parse_taxa(taxid)
-            taxonomy = { rank: tax.find_scientific_name(taxid) for rank, taxid 
-                    in lineage }
+            taxonomy = {rank: tax.find_scientific_name(taxid) for rank, taxid
+                    in lineage}
         else:
             taxonomy = {}
     else:
         raise TypeError('Sequence type needs to be specified as one of faa/fna/gbk')
-    baseName = os.path.basename(fasta).split('.')[0]
+    basename = os.path.basename(fasta).split('.')[0]
     if not name:
-        name = baseName
+        name = basename
     gene_matches = get_matches(faa, name, hmm, evalue)
     if gene_matches:
-        neighbour_find = findGeneNeighbourhood(fasta, faa, name, seqLength, 
-                gene_matches)
+        neighbour_find = findGeneNeighbourhood(fasta, faa, name, seqLength,
+                                               gene_matches)
         neighbours = neighbour_find.find_minimum_distance()
         # get product names if possible
         if neighbours:
-            for gene, match in gene_matches.items():
+            for gene, _ in gene_matches.items():
                 neighbours = get_neighbour_products(faa, fasta, neighbours, gene)
                 gene_matches[gene].append(extract_protein(faa, gene))
                 gene_matches[gene].append(neighbours[gene])
     else:
         gene_matches['-'].append(['-', '-'])
-    # make an entry of empty results  
-    compile_results(name, gene_matches, taxid, taxonomy, fasta, seqType, faa, q, 
-            seqLength, GC, gen_directory=gen_directory, c_bool=c_bool,
-            c_out=c_out)
+    # make an entry of empty results
+    compile_results(name, gene_matches, taxid, taxonomy, fasta, seqType, faa, q,
+                    seqLength, GC, gen_directory=gen_directory, c_bool=c_bool,
+                    c_out=c_out)
     return
 
 def get_neighbour_products(faa, fasta, neighbours, gene):
@@ -153,26 +153,26 @@ def get_neighbour_products(faa, fasta, neighbours, gene):
             neighbours[gene].append(None)
             continue
         neighbour_product = None
-        for prod in find_gbk_product(fasta, target=neighbours[gene][i][0], 
-                unique=True):
+        for prod in find_gbk_product(fasta, target=neighbours[gene][i][0],
+                                     unique=True):
             neighbour_product = prod
         if not neighbour_product:
             cprint(gene, "magenta")
             print(neighbours[gene][i][0])
             print(faa)
-            neighbour_faa = extract_protein(faa, 
-                    neighbours[gene][i][0], write=True)
+            neighbour_faa = extract_protein(faa, neighbours[gene][i][0],
+                                            write=True)
             # new func that takes extracted protein and diamond
             # against uniprot => extracts protein name
             blast = diamondBlast('/fast/uniprot', '_')
             print(neighbour_faa)
             try:
                 blast.perform_blast(neighbour_faa, '--outfmt', '6',
-                        'sseqid', 'evalue', 'bitscore', 'ppos',
-                        'salltitles', evalue='1e-10')
+                                    'sseqid', 'evalue', 'bitscore', 'ppos',
+                                    'salltitles', evalue='1e-10')
             except (TypeError, IndexError):
-                out = [name, fasta, faa, neighbours[gene][i][0]]
-                cprint(*out, "red", sep='\t')
+                out = [fasta, faa, neighbours[gene][i][0]]
+                cprint(out, "red")
             neighbours[gene].append(blast.get_protein_name())
         else:
             neighbours[gene].append(''.join(neighbour_product))
@@ -191,8 +191,8 @@ def compile_results(name, gene_matches, taxid, taxonomy, fasta, seqType, faa, q,
         result.update(taxonomy)
         # try to output the gene-file, pass if no genes were found
         try:
-            result['gene_path'] = os.path.abspath(gen_directory + '/' + 
-                    match[1][0].name + ".faa")
+            result['gene_path'] = os.path.abspath(gen_directory + '/' +
+                                                  match[1][0].name + ".faa")
             q.put(match[1])
         except IndexError:
             pass
@@ -229,23 +229,23 @@ def _listener(q, headers, outfile='-', gen_directory="protein_matches"):
             out_object = q.get()
             if out_object == "done":
                 break
-            elif type(out_object) is dict:
+            elif isinstance(out_object, dict):
                 for header in headers:
                     try:
                         handle.write(out_object[header.lower()] + '\t')
                     except (KeyError, TypeError):
                         handle.write('-' + '\t')
                 handle.write('\n')
-            if type(out_object) is list:
+            if isinstance(out_object, list):
                 # write aminoacid sequence
                 for seq_object in out_object:
                     try:
                         SeqIO.write(seq_object, gen_directory + '/' +
-                                seq_object.name + ".faa", "fasta")
+                                    seq_object.name + ".faa", "fasta")
                     except (IOError, AttributeError):
-                        cprint("Unable to output sequence: " + seq_object, "red", 
-                                file=sys.stderr)
-            elif type(out_object) is tuple:
+                        cprint("Unable to output sequence: " + seq_object, "red",
+                               file=sys.stderr)
+            elif isinstance(out_object, tuple):
                 for head in out_object:
                     handle.write(head + "\t")
                 handle.write("\n")
@@ -255,7 +255,7 @@ def _listener(q, headers, outfile='-', gen_directory="protein_matches"):
 
 @contextmanager
 def open_stdout(outfile):
-    """Generator dynamically allows printing to file or stdout when used as open 
+    """Generator dynamically allows printing to file or stdout when used as open
     with 'with'. No filename or '-' results in stdout, any other name will be
     used as filename to be written"""
     if outfile and outfile != '-':
@@ -272,10 +272,10 @@ def open_stdout(outfile):
 def get_matches(faa, name, hmm, evalue=1e-20):
     """Retrieves markers in hmm from the given proteome"""
     comp = calcCompleteness(faa, re.sub('\/', '', name), hmm, evalue)
-    foundmatches, dupmatches, totl = comp.get_completeness()
-    print(name + ': ' + str(foundmatches) )
+    foundmatches, _, _ = comp.get_completeness()
+    print(name + ': ' + str(foundmatches))
     # ensure unique, best match for each hmm
-    # but allow infinite gene matches for each hmm 
+    # but allow infinite gene matches for each hmm
     gene_matches = defaultdict(list)
     try:
         for matche, match in foundmatches.items():
@@ -290,15 +290,15 @@ def get_matches(faa, name, hmm, evalue=1e-20):
     return gene_matches
 
 def extract_protein(faa, gene_tag, write=None):
-    """Extract the protein sequences from a given proteome given a dict of 
+    """Extract the protein sequences from a given proteome given a dict of
     gene names"""
-    protein_list = [ seq for seq in SeqIO.parse(faa, "fasta") if seq.id in
-            gene_tag ]
+    protein_list = [seq for seq in SeqIO.parse(faa, "fasta") if seq.id in
+                    gene_tag]
     if write:
         print(protein_list)
         for protein in protein_list:
-            SeqIO.write(protein, NEIGHBOUR_DIR + '/' + protein.name + '.faa', 
-                    'fasta')
+            SeqIO.write(protein, NEIGHBOUR_DIR + '/' + protein.name + '.faa',
+                        'fasta')
             return NEIGHBOUR_DIR + '/' + protein.name + '.faa'
     return protein_list
 
@@ -316,7 +316,7 @@ def get_contigs_gbk(gbk, name=None):
     """Extracts all sequences from gbk file, returns filename"""
     handle = open(gbk, mode='r')
     if not name:
-        name = os.basename(gbk).split('.')[0]
+        name = os.path.basename(gbk).split('.')[0]
     out_handle = open(name, mode='w')
     for seq in SeqIO.parse(handle, "genbank"):
         out_handle.write(">" + seq.id + "\n")
@@ -325,7 +325,7 @@ def get_contigs_gbk(gbk, name=None):
     return name
 
 def find_gbk_product(gbk, target=True, unique=False):
-    """Matches target locus tag in given .gbk file. Yields any matched 
+    """Matches target locus tag in given .gbk file. Yields any matched
     product. Yields all products in gbk if no target given"""
     # hacky index
     n = 0
@@ -343,44 +343,44 @@ def find_gbk_product(gbk, target=True, unique=False):
 
 def init_results_table(q, outfile=None):
     headers = (
-                'Match',
-                'Gene',
-                'Evalue',
-                'Name',
-                'TaxID',
-                'Superkingdom',
-                'Phylum',
-                'Class',
-                'Order',
-                'Family',
-                'Genus',
-                'Species',
-                'Gene_path',
-                'Genome_path',
-                'Proteome_path',
-                'CRISPR',
-                'CRISPR_path',
-                'Genome_length',
-                'GC-content',
-                'Forward_neighbour',
-                'Forward_distance',
-                'Forward_product',
-                'Reverse_neighbour',
-                'Reverse_distance',
-                'Reverse_product',
-                )
+        'Match',
+        'Gene',
+        'Evalue',
+        'Name',
+        'TaxID',
+        'Superkingdom',
+        'Phylum',
+        'Class',
+        'Order',
+        'Family',
+        'Genus',
+        'Species',
+        'Gene_path',
+        'Genome_path',
+        'Proteome_path',
+        'CRISPR',
+        'CRISPR_path',
+        'Genome_length',
+        'GC-content',
+        'Forward_neighbour',
+        'Forward_distance',
+        'Forward_product',
+        'Reverse_neighbour',
+        'Reverse_distance',
+        'Reverse_product',
+        )
     if outfile and not outfile == '-':
         headers = tuple(headers)
         q.put(headers)
     else:
-        with open_stdout(outfile) as handle:    
+        with open_stdout(outfile) as handle:
             writer = csv.writer(handle, delimiter='\t')
             writer.writerow(headers)
     return headers
 
 def find_CRISPRs(fna, name=None):
-    """Uses CRISPR recognition tool (CRT) to find putative CRISPR repeats in 
-    given fna file, returns out-table, as well as True / False for positive / 
+    """Uses CRISPR recognition tool (CRT) to find putative CRISPR repeats in
+    given fna file, returns out-table, as well as True / False for positive /
     negative result."""
     input_handle = open(fna, mode='r')
     if name:
@@ -389,13 +389,13 @@ def find_CRISPRs(fna, name=None):
         baseName = os.path.basename(gbkfile).split('.')[0]
         outfile = baseName + ".cout"
         output_handle = outfile
-    run_fmt = [ 'java', '-cp', 'CRT1.2-CLI.jar', 'crt', fna, output_handle ]
+    run_fmt = ['java', '-cp', 'CRT1.2-CLI.jar', 'crt', fna, output_handle]
     return_proc = subprocess.run(run_fmt, stdout=subprocess.DEVNULL)
     errcode = return_proc.returncode
     if errcode > 0:
         print(return_proc)
         raise RuntimeError('Something went wrong with CRISPR finder for file %s'
-                % fna)
+                           % fna)
     with open(output_handle) as out:
         for line in out:
             if re.match("CRISPR [0-9]", line):
@@ -411,30 +411,30 @@ def main():
             investigate presence of RAYT via HMMs. Attempt to categorize mathces
             and investigate flanking genes and 16mers.""")
 
-    parser.add_argument("fastaList", help="""Sequence(s) along with type (fna, 
+    parser.add_argument("fastaList", help="""Sequence(s) along with type (fna,
             faa, gbk) provided in a tabular format""")
     parser.add_argument("-o", "--outfile", required=False, default="-",
             help="""Filename to save results. Otherwise prints to stdout.""")
     parser.add_argument("--gendir", required=False, default="protein_matches",
             help="""Directory in which to store matched protein sequences""")
     parser.add_argument("--hmms", required=False, default=False,
-            help="""Specifies a set of HMMs to be used for completeness check 
+            help="""Specifies a set of HMMs to be used for completeness check
                         or linkage analysis""")
     parser.add_argument("--taxa", required=False, help="""Query specific taxonomic
             group, requires a csv of the appropriate group from the NCBI genome
             browser""")
-    parser.add_argument("--glist", required=False, help="""Genome list in csv 
+    parser.add_argument("--glist", required=False, help="""Genome list in csv
             format from the NCBI genome browser, required with '--taxa' 
                         argument""")
     parser.add_argument("--crispr", required=False, action='store_true',
-            help="""Flag to attempt to assign CRISPR systems within examined 
+            help="""Flag to attempt to assign CRISPR systems within examined
             genomes using CRISPR Recognition Tool (CRT).""")
-    parser.add_argument("--summary", required=False, help="""Attempts to provide 
+    parser.add_argument("--summary", required=False, help="""Attempts to provide
             a summary of pre-exist results file. Provide a file of column(s) to 
             be summarised and optionally a selection column with a string to 
             be matched within the selection column""")
     parser.add_argument("--threads", required=False, default=1, type=int,
-            help="""Number of threads to be used concurrently""")
+                        help="""Number of threads to be used concurrently""")
     args = parser.parse_args()
 
     try:
@@ -451,8 +451,8 @@ def main():
     tax = parseTaxonomy()
 
     with open(args.fastaList) as seq_file:
-        inputSeqs = [seq.strip().split('\t') for seq in seq_file if not 
-                    re.match('#|\n', seq)]
+        input_seqs = [seq.strip().split('\t') for seq in seq_file if not
+                      re.match('#|\n', seq)]
 
     manager = mp.Manager()
     q = manager.Queue()
@@ -460,13 +460,13 @@ def main():
     pool = mp.Pool(processes=int(args.threads) + 1)
     # init listener here
     listener = pool.apply_async(_listener, (q, headers, args.outfile, args.gendir))
-    
     jobs = []
-    for i in inputSeqs: 
+    for i in input_seqs:
         if len(i) == 2:
             i.append(None)
-        job = pool.apply_async(_worker, (i[0], i[1], i[2], args.hmms, q, 
-            args.gendir, tax), {"crispr":args.crispr})
+        job = pool.apply_async(_worker, (i[0], i[1], i[2], args.hmms, q,
+                                         args.gendir, tax),
+                               {"crispr":args.crispr})
         jobs.append(job)
     # get() all processes to catch errors
     for job in jobs:
