a5c77ee82fb6070ecd6defe581092c1532240b79
eric.hugoson <hugoson@evolbio.mpg.de>
2019-06-04 18:23:23 +0200

    implemented all v all blast

--- minerva/cluster.py
+++ minerva/cluster.py
@@ -2,11 +2,19 @@
 
 import sys
 import subprocess
+import pandas as pd
+from Bio import SeqIO
 
-from minvera import diamondBlast
+from minerva import diamondBlast
 
 class clusterProteins():
     def __init__(self, result_tab, threads=1):
+        df = pd.read_csv(result_tab, sep='\t')
+        paths = df['Forward_path'].values.tolist()
+        paths = paths + df['Reverse_path'].values.tolist()
+        print(paths)
+        self.blast_all(paths)
+        return
         with open(result_tab) as f:
             self.result_tab = [entry.strip().split('\t') for entry in f]
         for n, entry in enumerate(self.result_tab[1]):
@@ -20,9 +28,24 @@ class clusterProteins():
                               self.result_tab}
 
     def blast_all(self, files):
+        filename = 'all_neighbours.faa'
+        handle = open(filename, 'w+')
         # cat all files to single file
-        subprocess.run('cat'
+        sequences = [SeqIO.parse(seq, 'fasta') for seq in files]
+        for sequence in sequences:
+            for seq in sequence:
+                handle.write('>' + seq.id + '\n')
+                handle.write(str(seq.seq) + '\n')
+        handle.close()
         # makedb from cat file
+        makedb = ['diamond', 'makedb', '--in', filename, '-d', filename]
+        db = subprocess.run(makedb)
+        if db.returncode > 0:
+            print("Failed to makedb")
+            return None
+        blast = diamondBlast(filename, filename)
+        blast_result = blast.perform_blast(filename, evalue="1e-2")
+        print(blast_result)
         # blast cat file against db
         # return scores
         pass
