3f303c0d72984bb3e1aa90ca241eda647eef35e0
Eric Hugoson <eric@hugoson.org>
2019-06-07 11:39:13 +0200

    integrated clustering

--- minerva/__init__.py
+++ minerva/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ['parse_mineva_map', 'parse_taxonomy', 'find_gene_neighbourhood', 'diamondblast'] 
+__all__ = ['parse_mineva_map', 'parse_taxonomy', 'find_gene_neighbourhood',
+           'diamondblast', 'cluster']
 
 from parse_mineva_map import parseMapFile
-from parse_taxonomy import parseTaxonomy
+from arse_taxonomy import parseTaxonomy
 from find_gene_neighbourhood import findGeneNeighbourhood
 from diamondblast import diamondBlast
+from cluster import clusterProteins
